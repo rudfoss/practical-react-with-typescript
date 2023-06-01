9f335f958a0b72ad818b1a2140bf18d12945aaff
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2023-06-01 08:57:52 +0200

    Fixed text field styling

--- apps/useradmin-wfe/src/TextField.tsx
+++ apps/useradmin-wfe/src/TextField.tsx
@@ -1,18 +1,17 @@
 import styled from "@emotion/styled"
 import React, { useId } from "react"
 
-const StyledLabel = styled.label`
-	display: block;
+const Container = styled.div`
+	padding: 8px;
+	margin-bottom: 8px;
 `
-
-interface StyledInputProps {
-	isRed: boolean
-}
-const StyledInput = styled.input<StyledInputProps>`
-	padding: 16px;
-	border: 3px solid #ccc;
-	border-radius: 6px;
-	background-color: ${(props) => (props.isRed ? "red" : "aqua")};
+const Label = styled.label`
+	display: block;
+	margin-bottom: 8px;
+`
+const Input = styled.input`
+	display: block;
+	width: 100%;
 `
 
 interface TextFieldProps {
@@ -23,22 +22,15 @@ interface TextFieldProps {
 
 export const TextField = (props: TextFieldProps) => {
 	const id = useId()
-	const isTooLong = props.value.length > 10
 
 	const onInputChange: React.ChangeEventHandler<HTMLInputElement> = (evt) => {
 		props.onChange(evt.target.value)
 	}
 
 	return (
-		<>
-			<StyledLabel htmlFor={id}>{props.label}</StyledLabel>
-			<StyledInput
-				isRed={isTooLong}
-				id={id}
-				type="text"
-				value={props.value}
-				onChange={onInputChange}
-			/>
-		</>
+		<Container>
+			<Label htmlFor={id}>{props.label}</Label>
+			<Input id={id} type="text" value={props.value} onChange={onInputChange} />
+		</Container>
 	)
 }
