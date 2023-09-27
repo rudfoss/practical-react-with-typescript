34be996e9835128a88064ed747f162986dfd28c6
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2023-09-27 11:16:24 +0200

    State management in Text Field

--- apps/useradmin-wfe/src/TextField.tsx
+++ apps/useradmin-wfe/src/TextField.tsx
@@ -8,15 +8,19 @@ export const TextField = () => {
 	const id = useId()
 	const [value, setValue] = useState("...")
 
-	const onChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+	const onInputChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
 		setValue(evt.target.value)
+	}
+	const clearValue = () => {
+		setValue("")
 	}
 
 	return (
 		<div>
 			<label htmlFor={id}>Enter some text</label>
-			<input id={id} type="text" onChange={onChange} />
+			<input id={id} type="text" value={value} onChange={onInputChange} />
 			<p>{value}</p>
+			<button onClick={clearValue}>Clear</button>
 		</div>
 	)
 }
