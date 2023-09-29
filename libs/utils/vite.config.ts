555a5d785926f15a8b1991a45a795e674a56336b
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2023-09-29 16:22:46 +0200

    Partial test

--- libs/utils/vite.config.ts
+++ libs/utils/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-	cacheDir: "../../node_modules/.vite/user-admin",
+	cacheDir: "../../node_modules/.vite/utils",
 
 	plugins: [react(), nxViteTsPaths()],
 
