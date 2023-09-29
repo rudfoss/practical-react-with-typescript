057015e0cacb23794dde535190fd0b31bd634d5d
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2023-09-29 09:50:50 +0200

    useWindowTitle

--- apps/useradmin-wfe/src/pages/UserDetailsPage.tsx
+++ apps/useradmin-wfe/src/pages/UserDetailsPage.tsx
@@ -1,9 +1,11 @@
 import { useParams } from "react-router-dom"
 
+import { useWindowTitle } from "@prwt/tasks"
 import { UserDetailsById } from "@prwt/user-admin"
 
 export const UserDetailsPage = () => {
 	const { userId } = useParams<"userId">()
+	useWindowTitle(`User ${userId}`)
 
 	return <UserDetailsById userId={userId} />
 }
--- apps/useradmin-wfe/src/pages/UsersListPage.tsx
+++ apps/useradmin-wfe/src/pages/UsersListPage.tsx
@@ -1,5 +1,7 @@
+import { useWindowTitle } from "@prwt/tasks"
 import { UsersTableFromConstants } from "@prwt/user-admin"
 
 export const UsersListPage = () => {
+	useWindowTitle("Users")
 	return <UsersTableFromConstants />
 }
