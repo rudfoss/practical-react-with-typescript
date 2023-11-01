25f487fe4eaeb3ac7a0c7f753aa059c255f4e6a5
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2023-11-01 16:16:23 +0100

    Missing main file

---
+++ apps/useradmin-wfe/src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react"
+import * as ReactDOM from "react-dom/client"
+
+import "modern-normalize/modern-normalize.css"
+
+import { App } from "./App"
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
+root.render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+)
