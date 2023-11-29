2591f2158dfdb3e202a299c64f88c4eded3d3bde
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2023-11-29 10:09:34 +0100

    Component Basics

--- apps/store-front/src/App.tsx
+++ apps/store-front/src/App.tsx
@@ -1,10 +1,26 @@
+import React from "react"
+
+const MyComponentArrow = () => {
+	return <strong>MyComponentArrow</strong>
+}
+const MyComponentPlain = () => {
+	return React.createElement("strong", {}, "MyComponentPlain")
+}
+
+function MyComponent() {
+	return <strong>MyComponent</strong>
+}
+
+const arr = [4, 2, 5, 7]
+arr.filter((v) => v < 5)
+
 export const App = () => {
 	return (
 		<h1>
 			<span role="img" aria-label="Rocket">
 				🚀
 			</span>{" "}
-			It works
+			<MyComponentPlain />
 		</h1>
 	)
 }
