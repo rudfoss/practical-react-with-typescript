20107c205c36538543da7bddb9d9dc84a9827868
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2023-11-29 15:59:07 +0100

    ListProductNames

--- apps/store-front/src/ListProductNames.tsx
+++ apps/store-front/src/ListProductNames.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react"
+import { useState } from "react"
 
 import { Product } from "./products"
 
@@ -7,13 +7,34 @@ export interface ListProductNamesProps {
 }
 
 export const ListProductNames = ({ products }: ListProductNamesProps) => {
+	const [selectedProduct, setSelectedProduct] = useState<{
+		product: Product
+		idx: number
+	}>()
+
+	const selectProduct = (product: Product, idx: number) => () => {
+		setSelectedProduct({ product, idx })
+	}
+
 	return (
 		<>
 			<h3>List of products</h3>
 			<p>There are {products.length} product(s)</p>
+			{selectedProduct && (
+				<p>
+					<strong>Selected product {selectedProduct.idx}:</strong>{" "}
+					{selectedProduct.product.title}
+				</p>
+			)}
 			<ul>
-				{products.map((product) => (
-					<li key={product.id}>{product.title}</li>
+				{products.map((product, idx) => (
+					<li
+						key={product.id}
+						onClick={selectProduct(product, idx)}
+						style={{ cursor: "pointer" }}
+					>
+						{product.title}
+					</li>
 				))}
 			</ul>
 		</>
