17523feb898484d9d1e5d4df081498044df4fa57
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2023-11-30 11:57:13 +0100

    ProductsTable2

--- apps/store-front/src/ProductsTable.tsx
+++ apps/store-front/src/ProductsTable.tsx
@@ -1,4 +1,20 @@
+import { useMemo, useState } from "react"
+
 import { Product } from "./products"
+
+type SortBy = "title" | "price" | "category" | "rating"
+// type X = keyof Pick<Product, "title" | "price" | "category" | "rating">
+type SortDirection = "asc" | "desc"
+
+const getSortIcon = (
+	expectedSortBy: SortBy,
+	sortDirection: SortDirection,
+	sortBy?: SortBy
+) => {
+	if (!sortBy) return ""
+	if (sortBy !== expectedSortBy) return ""
+	return sortDirection === "asc" ? "⬇️" : "⬆️"
+}
 
 export interface ProductsTableProps {
 	products: Product[]
@@ -6,23 +22,70 @@ export interface ProductsTableProps {
 }
 
 export const ProductsTable = ({ products, onDelete }: ProductsTableProps) => {
+	const [sortBy, setSortBy] = useState<SortBy>()
+	const [sortDirection, setSortDirection] = useState<SortDirection>("asc")
+
+	const sortedProducts = useMemo(() => {
+		if (!sortBy) return products
+		const productsToSort = products.slice(0)
+		productsToSort.sort((a, b) => {
+			let result = 0
+
+			switch (sortBy) {
+				case "title":
+					result = a.title.localeCompare(b.title)
+					break
+				case "category":
+					result = a.category.localeCompare(b.category)
+					break
+				case "price":
+					result = a.price - b.price
+					break
+				case "rating":
+					result = a.rating - b.rating
+					break
+			}
+
+			return sortDirection === "asc" ? result : result * -1
+		})
+
+		return productsToSort
+	}, [products, sortBy, sortDirection])
+
 	const deleteProduct = (product: Product) => () => {
 		onDelete(product)
+	}
+
+	const changeSortBy = (newSortBy: SortBy) => () => {
+		if (sortBy === newSortBy) {
+			setSortDirection(sortDirection === "asc" ? "desc" : "asc")
+			return
+		}
+		setSortBy(newSortBy)
+		setSortDirection("asc")
 	}
 
 	return (
 		<table>
 			<thead>
 				<tr>
-					<th>Title</th>
-					<th>Price</th>
-					<th>Category</th>
-					<th>Rating</th>
+					<th onClick={changeSortBy("title")}>
+						Title {getSortIcon("title", sortDirection, sortBy)}
+					</th>
+					<th onClick={changeSortBy("price")}>
+						Price {getSortIcon("price", sortDirection, sortBy)}
+					</th>
+					<th onClick={changeSortBy("category")}>
+						Category {getSortIcon("category", sortDirection, sortBy)}
+					</th>
+					<th onClick={changeSortBy("rating")}>
+						Rating {getSortIcon("rating", sortDirection, sortBy)}
+					</th>
 					<th></th>
 				</tr>
 			</thead>
 			<tbody>
-				{products.map((product) => (
+				{sortedProducts.map((product) => (
 					<tr key={product.id}>
 						<td>{product.title}</td>
 						<td>{product.price}</td>
