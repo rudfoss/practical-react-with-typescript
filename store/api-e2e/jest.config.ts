44d91490df1317a5245960fd9e4b03b188753a0f
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2024-01-29 09:50:18 +0100

    Working jest tests

--- store/api-e2e/jest.config.ts
+++ store/api-e2e/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-	displayName: "store-e2e",
+	displayName: "store-api-e2e",
 	preset: "../../jest.preset.js",
 	globalSetup: "<rootDir>/src/support/global-setup.ts",
 	globalTeardown: "<rootDir>/src/support/global-teardown.ts",
@@ -15,5 +15,5 @@ export default {
 		]
 	},
 	moduleFileExtensions: ["ts", "js", "html"],
-	coverageDirectory: "../../coverage/store-e2e"
+	coverageDirectory: "../../coverage/store/api"
 }
--- store/api/jest.config.ts
+++ store/api/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-	displayName: "store",
+	displayName: "store-api",
 	preset: "./jest.preset.js",
 	testEnvironment: "node",
 	transform: {
 		"^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }]
 	},
 	moduleFileExtensions: ["ts", "js", "html"],
-	coverageDirectory: "../../coverage/apis/store"
+	coverageDirectory: "../../coverage/apis/store-api"
 }
