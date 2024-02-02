3836d46fe6a841dfe53a3909551616ad04977a95
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2024-02-02 08:34:26 +0100

    Fixed tests

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,7 @@
+// import nxPreset from "@nx/jest/preset/index.js"
+
+// export default nxPreset
+
 const nxPreset = require("@nx/jest/preset").default
 
 module.exports = { ...nxPreset }
--- userdb/api/jest.config.ts
+++ userdb/api/jest.config.ts
@@ -2,7 +2,7 @@
 /* eslint-disable */
 export default {
 	displayName: "userdb-api",
-	preset: "./jest.preset.js",
+	preset: "../../jest.preset.js",
 	testEnvironment: "node",
 	transform: {
 		"^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }]
