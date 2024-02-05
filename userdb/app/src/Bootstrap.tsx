f231cb78af71186255cd371c73081e4a7c43d3ce
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2024-02-05 14:15:42 +0100

    Added home link

--- userdb/app/src/Bootstrap.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react"
-
-export interface BootstrapProps {
-	children: ReactNode
-}
-
-export const Bootstrap = ({ children }: BootstrapProps) => {
-	return children
-}
--- userdb/app/src/pages/UsersPage.tsx
+++ userdb/app/src/pages/UsersPage.tsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom"
+
 import { StaticUsersList } from "@react-workshop/userdb-libs-users"
 
 export const UsersPage = () => {
-	return <StaticUsersList />
+	return (
+		<>
+			<Link to="/">Home</Link>
+			<StaticUsersList />
+		</>
+	)
 }
