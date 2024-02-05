d296e95b2c8358dd6d83a6d26240992609099295
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2024-02-05 13:53:28 +0100

    Moved login form

--- userdb/app/src/App.tsx
+++ userdb/app/src/App.tsx
@@ -1,31 +1,5 @@
-import { useState } from "react"
-
-import { ChoiceField, ChoiceFieldChoice } from "@react-workshop/fields"
-import { StaticGroupList } from "@react-workshop/userdb-libs-groups"
-import { StaticUser, StaticUsersList, staticUsers } from "@react-workshop/userdb-libs-users"
-
-const staticUserChoices = staticUsers.map(
-	(user): ChoiceFieldChoice<StaticUser> => ({
-		data: user,
-		label: user.displayName,
-		value: user.id
-	})
-)
+import { LoginForm } from "@react-workshop/userdb-libs-auth"
 
 export const App = () => {
-	const [choiceValue, setChoiceValue] = useState<ChoiceFieldChoice<StaticUser>>()
-
-	return (
-		<>
-			<StaticGroupList />
-			<StaticUsersList />
-			<ChoiceField
-				label="Pick a user"
-				options={staticUserChoices}
-				value={choiceValue}
-				onChange={setChoiceValue}
-				variant="select"
-			/>
-		</>
-	)
+	return <LoginForm onLogin={() => console.log("")} />
 }
