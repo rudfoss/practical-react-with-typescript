12c2b550c28f3769b42dd0c036247cae48b5c77d
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2024-02-05 10:03:55 +0100

    Hoisted state

--- userdb/app/src/App.tsx
+++ userdb/app/src/App.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react"
+
 import { Header } from "./Header"
 import { TextField } from "./TextField"
 
 export const App = () => {
+	const [heading, setHeading] = useState("Hello world!")
+
 	return (
 		<>
-			<Header>Hello world!</Header>
-			<TextField label="Header text" />
+			<Header>{heading}</Header>
+			<TextField value={heading} onChange={setHeading} label="Header text" />
 		</>
 	)
 }
--- userdb/app/src/TextField.tsx
+++ userdb/app/src/TextField.tsx
@@ -1,16 +1,23 @@
-import { useId } from "react"
+import { ChangeEvent, useId } from "react"
 
 export interface TextFieldProps {
+	value: string
+	onChange: (newValue: string) => unknown
+
 	label: string
 }
 
-export const TextField = ({ label }: TextFieldProps) => {
+export const TextField = ({ value, onChange, label }: TextFieldProps) => {
 	const id = useId()
+
+	const onInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+		onChange(event.currentTarget.value)
+	}
 
 	return (
 		<div>
 			<label htmlFor={id}>{label}</label>
-			<input type="text" id={id} />
+			<input type="text" id={id} value={value} onChange={onInputChange} />
 		</div>
 	)
 }
