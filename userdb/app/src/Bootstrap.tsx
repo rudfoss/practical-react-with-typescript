80390606890324f823a654f492634bd6a30936e3
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2024-02-07 08:36:06 +0100

    Set up React Query

--- userdb/app/src/Bootstrap.tsx
+++ userdb/app/src/Bootstrap.tsx
@@ -1,7 +1,11 @@
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { ReactNode } from "react"
 
 import { ProvideFieldsService } from "@react-workshop/fields"
 import { ProvideHeaderService } from "@react-workshop/ui"
+
+const queryClient = new QueryClient()
 
 export interface BootstrapProps {
 	children: ReactNode
@@ -9,8 +13,11 @@ export interface BootstrapProps {
 
 export const Bootstrap = ({ children }: BootstrapProps) => {
 	return (
-		<ProvideFieldsService>
-			<ProvideHeaderService>{children}</ProvideHeaderService>
-		</ProvideFieldsService>
+		<QueryClientProvider client={queryClient}>
+			<ProvideFieldsService>
+				<ProvideHeaderService>{children}</ProvideHeaderService>
+			</ProvideFieldsService>
+			<ReactQueryDevtools />
+		</QueryClientProvider>
 	)
 }
