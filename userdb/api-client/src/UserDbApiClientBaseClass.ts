75c299dd5648760f71f4b515aa8868e99f4da932
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2024-02-07 07:46:48 +0100

    Updated client generation

---
+++ userdb/api-client/src/UserDbApiClientBaseClass.ts
@@ -0,0 +1,30 @@
+export type OptionTransformer = (options: RequestInit) => Promise<RequestInit>
+
+/**
+ * A very simple base class that allows statically adding a transformer.
+ */
+export class UserDbApiClientBaseClass {
+	public async transformOptions(options: RequestInit) {
+		return UserDbApiClientBaseClass.globalOptionsTransformer?.(options)
+	}
+
+	public static globalOptionsTransformer?: OptionTransformer
+}
+
+/**
+ * Set or remove the bearer token authorization header on the global parent class.
+ * @param sessionToken
+ * @returns
+ */
+export const setBearerToken = (sessionToken?: string) => {
+	if (!sessionToken) {
+		UserDbApiClientBaseClass.globalOptionsTransformer = undefined
+		return
+	}
+
+	UserDbApiClientBaseClass.globalOptionsTransformer = async (options) => {
+		options.headers = new Headers(options.headers)
+		options.headers.set("Authorization", `Bearer ${sessionToken}`)
+		return options
+	}
+}
--- userdb/api-client/src/index.ts
+++ userdb/api-client/src/index.ts
@@ -1,2 +1,2 @@
 export * from "./UserDbApiClient"
-export * from "./ClientBaseClass"
+export * from "./UserDbApiClientBaseClass"
