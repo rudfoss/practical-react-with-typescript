0d4d5eccc23283712bf9d36c1793d62dae01462b
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2024-02-07 12:03:28 +0100

    Added disable support to the login form

--- userdb/libs/auth/src/LoginForm/LoginForm.tsx
+++ userdb/libs/auth/src/LoginForm/LoginForm.tsx
@@ -3,14 +3,16 @@ import { FormEvent, useState } from "react"
 import { TextField } from "@react-workshop/fields"
 
 export interface LoginFormProps {
+	disabled?: boolean
 	onLogin: (username: string, password: string) => unknown
 }
 
-export const LoginForm = ({ onLogin }: LoginFormProps) => {
+export const LoginForm = ({ disabled, onLogin }: LoginFormProps) => {
 	const [username, setUsername] = useState("")
 	const [password, setPassword] = useState("")
 
 	const onSubmit = (event: FormEvent) => {
+		if (disabled) return
 		event.preventDefault()
 		onLogin(username, password)
 	}
@@ -22,10 +24,16 @@ export const LoginForm = ({ onLogin }: LoginFormProps) => {
 
 	return (
 		<form onSubmit={onSubmit}>
-			<TextField label="User name" value={username} onChange={setUsername} />
-			<TextField type="password" label="Password" value={password} onChange={setPassword} />
-			<input type="reset" value="Clear" onClick={onReset} />
-			<input type="submit" value="Log in" />
+			<TextField label="User name" value={username} onChange={setUsername} disabled={disabled} />
+			<TextField
+				type="password"
+				label="Password"
+				value={password}
+				onChange={setPassword}
+				disabled={disabled}
+			/>
+			<input type="reset" value="Clear" onClick={onReset} disabled={disabled} />
+			<input type="submit" value="Log in" disabled={disabled} />
 		</form>
 	)
 }
