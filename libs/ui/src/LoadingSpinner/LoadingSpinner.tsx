75b89394f657c1518ace859196c706a163047da3
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2024-02-07 13:09:02 +0100

    Centered spinner

--- libs/ui/src/LoadingSpinner/LoadingSpinner.tsx
+++ libs/ui/src/LoadingSpinner/LoadingSpinner.tsx
@@ -1,9 +1,25 @@
+import styled from "@emotion/styled"
+
 import spinner from "./netscape-loader.gif"
+
+const Container = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	img {
+		display: block;
+		flex: 0 0 auto;
+	}
+`
 
 export interface LoadingSpinnerProps {
 	size?: number
 }
 
 export const LoadingSpinner = ({ size = 32 }: LoadingSpinnerProps) => {
-	return <img alt="Loading spinner" src={spinner} width={32} height={32} />
+	return (
+		<Container>
+			<img alt="Loading spinner" src={spinner} width={32} height={32} />
+		</Container>
+	)
 }
