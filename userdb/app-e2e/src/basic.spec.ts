ed990059c34005a17869c9df2c90eb808c496bee
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2024-02-15 10:08:50 +0100

    Fixed broken test

--- userdb/app-e2e/src/basic.spec.ts
+++ userdb/app-e2e/src/basic.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test"
 
 test("sanity check", async ({ page }) => {
 	await page.goto("/")
-	expect(page.locator("#root > *")).not.toBeTruthy()
+	expect(page.locator("#root > *")).toBeTruthy()
 
 	// Expect h1 to contain a substring.
 	// expect(await page.locator("h1").textContent()).toContain("Hello world")
