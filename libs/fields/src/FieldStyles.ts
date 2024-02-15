285c0d74a75b7ae66d249d82341eebc844b825ae
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2024-02-15 14:58:54 +0100

    Reuse Styles

--- libs/fields/src/FieldStyles.ts
+++ libs/fields/src/FieldStyles.ts
@@ -7,8 +7,7 @@ export const Label = styled.label`
 	display: block;
 	padding-bottom: 4px;
 `
-export const LabelAfter = styled.label`
-	display: block;
+export const LabelAfter = styled(Label)`
 	padding-left: 4px;
 	min-height: 1.2rem;
 `
--- libs/ui/src/Header/Header.tsx
+++ libs/ui/src/Header/Header.tsx
@@ -6,6 +6,10 @@ const StyledHeader = styled.header`
 	border-bottom: 1px solid #000;
 	padding: 8px;
 	margin-bottom: 8px;
+
+	&::selection {
+		background-color: hotpink;
+	}
 `
 const H1 = styled.h1`
 	margin: 0;
