2e0a77f681d02e5656d64fd2759d8b8948a8a5a3
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2024-02-15 08:23:40 +0100

    Test for rendered content

--- userdb/app-e2e/src/basic.spec.ts
+++ userdb/app-e2e/src/basic.spec.ts
@@ -2,7 +2,8 @@ import { test, expect } from "@playwright/test"
 
 test("sanity check", async ({ page }) => {
 	await page.goto("/")
-	expect(page.locator("#root")).toBeTruthy()
+	// Testing that there is a <div id="root"> with some content inside it
+	expect(page.locator("#root > *")).toBeTruthy()
 
 	// Expect h1 to contain a substring.
 	// expect(await page.locator("h1").textContent()).toContain("Hello world")
