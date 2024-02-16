8f4c84b1e4257bd18cfcf10a213277f5a81637bf
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2024-02-16 08:05:02 +0100

    Improved header styling

--- libs/ui/src/Header/Header.tsx
+++ libs/ui/src/Header/Header.tsx
@@ -1,17 +1,24 @@
 import styled from "@emotion/styled"
 
-const H1 = styled.h1`
-	font-weight: bold;
-	font-family: monospace;
+const Container = styled.div`
+	border-bottom: 1px solid #000;
+	padding: 8px;
+	margin-bottom: 8px;
 
 	&::selection {
 		background-color: hotpink;
-		color: #fff;
 	}
+`
+const H1 = styled.h1`
+	margin: 0;
 `
 
 export interface HeaderProps {
 	children: string
 }
 
-export const Header = ({ children }: HeaderProps) => <H1>{children}</H1>
+export const Header = ({ children }: HeaderProps) => (
+	<Container>
+		<H1>{children}</H1>
+	</Container>
+)
