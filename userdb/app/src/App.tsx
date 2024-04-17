4a4ddd67b1ec67135431e33def80321e09d76623
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2024-04-17 10:09:27 +0200

    Basic component structure

--- userdb/app/src/App.tsx
+++ userdb/app/src/App.tsx
@@ -1,1 +1,21 @@
-export const App = () => <h1>👋 Hello there</h1>
+interface MyComponentProps {
+	greeting: string
+	suffix: string
+	padWithWhitespaces: number
+	hideSuffix?: boolean
+}
+
+const MyComponent = (props: MyComponentProps) => (
+	<h2>
+		MyComponent: {props.greeting}
+		{props.suffix}
+	</h2>
+)
+
+export const App = () => (
+	<h1>
+		👋 Hello there
+		<MyComponent greeting="Good morning" suffix="!" padWithWhitespaces={4} hideSuffix />
+		{/* React.createElement(MyComponent, {greeting: "Good morning", suffix:"!" }) */}
+	</h1>
+)
