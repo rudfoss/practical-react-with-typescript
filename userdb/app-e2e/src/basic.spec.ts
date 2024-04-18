3ca4de083c2cbbc1c05938019031104a324c31c7
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2024-04-18 11:02:09 +0200

    e2e testing

--- userdb/app-e2e/src/basic.spec.ts
+++ userdb/app-e2e/src/basic.spec.ts
@@ -2,9 +2,6 @@ import { test, expect } from "@playwright/test"
 
 test("sanity check", async ({ page }) => {
 	await page.goto("/")
-	// Testing that there is a <div id="root"> with some content inside it
-	expect(page.locator("#root > *")).toBeTruthy()
 
-	// Expect h1 to contain a substring.
-	// expect(await page.locator("h1").textContent()).toContain("Hello world")
+	expect(await page.locator("#root > h1").textContent()).toContain("Hello world")
 })
