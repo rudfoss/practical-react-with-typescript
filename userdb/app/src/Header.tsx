0e661e342be1165fe4bc32b1e795000d5184ac99
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2024-04-18 09:52:08 +0200

    Styling using @emotion/styled

--- userdb/app/src/Header.tsx
+++ userdb/app/src/Header.tsx
@@ -1,17 +1,21 @@
+import styled from "@emotion/styled"
 import { ReactNode } from "react"
 
-import classes from "./Header.module.css"
+interface StyledH1Props {
+	emphasize?: boolean
+}
+const StyledH1 = styled.h1<StyledH1Props>`
+	font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande", "Lucida Sans", Arial,
+		sans-serif;
+	border: 3px solid #000;
+	border-radius: 5px;
+`
 
 export interface HeaderProps {
 	emphasize?: boolean
 	children: ReactNode
 }
 
-export const Header = ({ emphasize, children }: HeaderProps) => {
-	const className = [classes.heading]
-	if (emphasize) {
-		className.push(classes.emphasized)
-	}
-
-	return <h1 className={className.join(" ")}>{children}</h1>
-}
+export const Header = ({ emphasize, children }: HeaderProps) => (
+	<StyledH1 emphasize={emphasize}>{children}</StyledH1>
+)
