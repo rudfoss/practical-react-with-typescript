091a7d0f28b7bde7b3a873e2b0b82a1e9f1cebf3
Thomas Haugland Rudfoss <thomas.rudfoss@gmail.com>
2024-12-04 15:59:41 +0100

    StaticGroupTable

--- userdb/app/src/App.tsx
+++ userdb/app/src/App.tsx
@@ -1,4 +1,15 @@
-import { ListGroups } from "./ListGroups"
-import { staticGroups } from "./staticGroups"
+import { useState } from "react"
 
-export const App = () => <ListGroups groups={staticGroups} />
+import { ListGroups } from "./ListGroups"
+import { StaticGroupTable } from "./StaticGroupTable"
+import { StaticGroup, staticGroups } from "./staticGroups"
+
+export const App = () => {
+  const [groups, setGroups] = useState<StaticGroup[]>(staticGroups)
+  return (
+    <>
+      <ListGroups groups={groups} />
+      <StaticGroupTable groups={groups} setGroups={setGroups} />
+    </>
+  )
+}
---
+++ userdb/app/src/StaticGroupTable.tsx
@@ -0,0 +1,55 @@
+import { arrayMove } from "@practical-react/utils"
+
+import { StaticGroup } from "./staticGroups"
+
+export interface StaticGroupTableProps {
+  groups: StaticGroup[]
+  setGroups: (groups: StaticGroup[]) => unknown
+}
+
+export const StaticGroupTable = ({
+  groups,
+  setGroups
+}: StaticGroupTableProps) => {
+  const moveUp = (index: number) => {
+    setGroups(arrayMove(groups, index, index - 1))
+  }
+  const moveDown = (index: number) => {
+    setGroups(arrayMove(groups, index, index + 1))
+  }
+  const deleteItem = (index: number) => {
+    setGroups(groups.filter((_, groupIndex) => groupIndex !== index))
+  }
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Display Name</th>
+          <th>Controls</th>
+        </tr>
+      </thead>
+      <tbody>
+        {groups.map((group, index) => (
+          <tr key={group.id}>
+            <td>{group.id}</td>
+            <td>{group.displayName}</td>
+            <td>
+              <button disabled={index === 0} onClick={() => moveUp(index)}>
+                ⬆️
+              </button>
+              <button
+                disabled={index >= groups.length - 1}
+                onClick={() => moveDown(index)}
+              >
+                ⬇️
+              </button>
+              <button onClick={() => deleteItem(index)}>🗑️</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
