b6792570fc7cf5512d26a94a370535705de8f74b
Thomas Haugland Rudfoss <thomas.rudfoss@gmail.com>
2024-12-04 14:44:40 +0100

    LoginForm

--- userdb/app/src/App.tsx
+++ userdb/app/src/App.tsx
@@ -1,27 +1,7 @@
-import { useState } from "react"
+import { LoginForm } from "./LoginForm"
 
-import { CheckboxField } from "./CheckboxField"
-import { PasswordField } from "./PasswordField"
-
-export const App = () => {
-  const [password, setPassword] = useState("")
-  const [isPasswordVisible, setIsPasswordVisible] = useState(false)
-
-  return (
-    <>
-      <PasswordField
-        label="Password"
-        minLength={8}
-        maxLength={128}
-        showPassword={isPasswordVisible}
-        value={password}
-        setValue={setPassword}
-      />
-      <CheckboxField
-        label="Show password"
-        value={isPasswordVisible}
-        setValue={setIsPasswordVisible}
-      />
-    </>
-  )
+const onLogin = (username: string, password: string) => {
+  alert(`Logging you in with ${username} and ${password}`)
 }
+
+export const App = () => <LoginForm onLogin={onLogin} />
---
+++ userdb/app/src/LoginForm.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react"
+
+import { CheckboxField } from "./CheckboxField"
+import { PasswordField } from "./PasswordField"
+import { TextField } from "./TextField"
+
+export interface LoginFormProps {
+  disabled?: boolean
+  onLogin: (username: string, password: string) => unknown
+}
+
+export const LoginForm = ({ onLogin, disabled }: LoginFormProps) => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false)
+  const isValid = username.length > 0 && password.length > 0
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    if (!isValid) return
+
+    onLogin(username, password)
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <TextField
+        label="User name"
+        value={username}
+        minLength={1}
+        setValue={setUsername}
+        disabled={disabled}
+      />
+      <PasswordField
+        minLength={1}
+        maxLength={128}
+        label="Password"
+        value={password}
+        setValue={setPassword}
+        disabled={disabled}
+        showPassword={isPasswordVisible}
+      />
+      <CheckboxField
+        label="Show password"
+        value={isPasswordVisible}
+        setValue={setIsPasswordVisible}
+        disabled={disabled}
+      />
+      <button disabled={!isValid || disabled}>Log in</button>
+    </form>
+  )
+}
