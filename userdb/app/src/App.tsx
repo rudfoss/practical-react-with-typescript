f2822748d7ebb7b84c66a2ed704b36c9421c8f4b
Thomas Haugland Rudfoss <thomas.rudfoss@gmail.com>
2025-04-23 13:26:01 +0200

    Hoist state

--- userdb/app/src/App.tsx
+++ userdb/app/src/App.tsx
@@ -1,14 +1,29 @@
+import { useState } from "react"
 import { Header } from "./Header"
 import { TextField } from "./TextField"
 
 export const App = () => {
+	const [firstName, setFirstName] = useState("")
+	const [lastName, setLastName] = useState("")
+
+	const clear = () => {
+		setFirstName("")
+		setLastName("")
+	}
+
 	return (
 		<>
 			<Header>
 				Hello <code>from</code> app
 			</Header>
-			<TextField label="First name" />
-			<TextField label="Last name" />
+			<TextField label="First name" value={firstName} onChange={setFirstName} />
+			<TextField label="Last name" value={lastName} onChange={setLastName} />
+			<button type="button" onClick={clear}>
+				Clear
+			</button>
+			<p>
+				Hei {firstName} {lastName}
+			</p>
 		</>
 	)
 }
--- userdb/app/src/TextField.tsx
+++ userdb/app/src/TextField.tsx
@@ -1,17 +1,29 @@
-import { useId } from "react"
+import { ChangeEvent, useId } from "react"
 
 export interface TextFieldProps {
 	label: string
 	disabled?: boolean
+
+	value: string
+	onChange: (newText: string) => unknown
 }
 
-export const TextField = ({ label, disabled }: TextFieldProps) => {
+export const TextField = ({
+	label,
+	disabled,
+	value,
+	onChange
+}: TextFieldProps) => {
 	const id = useId()
+
+	const updateValue = (eventObject: ChangeEvent<HTMLInputElement>) => {
+		onChange(eventObject.target.value)
+	}
 
 	return (
 		<div>
 			<label htmlFor={id}>{label}</label>
-			<input id={id} disabled={disabled} />
+			<input value={value} id={id} disabled={disabled} onChange={updateValue} />
 		</div>
 	)
 }
